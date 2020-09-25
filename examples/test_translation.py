0e001d15b84b441faf7b852c692bb830663bc417
GouMinghao <gmh2015@163.com>
2020-09-25 14:20:30 +0800

    update

--- examples/test_translation.py
+++ examples/test_translation.py
@@ -19,7 +19,7 @@ try:
 
     rob.set_payload(0, (0, 0, 0))
 
-    v = 0.2
+    v = 0.1
     a = 0.1
 
     pose = rob.getl()
@@ -32,17 +32,18 @@ try:
     print('origin pose:{}'.format(pose))
 
 
-    pose[0] = -0.5
-    pose[1] = 0
-    pose[2] = 0.35
+    # pose[0] = -0.5
+    # pose[1] = 0
+    # pose[2] = 0.35
 
-    pose[3] = 0
-    pose[4] = - math.pi
-    pose[5] = 0
+    # pose[3] = 0
+    # pose[4] = - math.pi
+    # pose[5] = 0
 
-    pose[0] = -0.45
-    pose[1] = 0
-    pose[2] = 0.45
+    pose[0] = -0.45 + 0.079 - 0.077
+    pose[1] = 0 - 0.136 
+    pose[2] = 0.45 - 0.035 - 0.40 + 0.16
+    # pose[2] = 0.45 
 
     pose[3] = 2.2214415
     pose[4] = 2.2214415
@@ -50,18 +51,28 @@ try:
 
     rob.movel(pose, acc=a, vel=v)
 
-    pose[0] = -0.5
-    pose[1] = 0
-    pose[2] = 0.23
+    # pose[0] = -0.5
+    # pose[1] = 0
+    # pose[2] = 0.23
 
-    pose[3] = 0
-    pose[4] = - math.pi
-    pose[5] = 0
+    # pose[3] = 0
+    # pose[4] = - math.pi
+    # pose[5] = 0
 
 
-    rob.movel(pose, acc=a, vel=v)
+    # rob.movel(pose, acc=a, vel=v)
     robotiqgrip.close_gripper()
 
+    # pose[0] = -0.45
+    # pose[1] = 0
+    # pose[2] = 0.45
+
+    # pose[3] = 2.2214415
+    # pose[4] = 2.2214415
+    # pose[5] = 0
+
+    # rob.movel(pose, acc=a, vel=v)
+    
     pose[0] = -0.45
     pose[1] = 0
     pose[2] = 0.45
@@ -72,15 +83,6 @@ try:
 
     rob.movel(pose, acc=a, vel=v)
     robotiqgrip.open_gripper()
-    # pose[0] = -0.45
-    # pose[1] = 0
-    # pose[2] = 0.45
-
-    # pose[3] = 2.2214415
-    # pose[4] = 2.2214415
-    # pose[5] = 0
-
-    rob.movel(pose, acc=a, vel=v)
 
     pose = rob.getl()
     print('actual pose:{}'.format(pose))
